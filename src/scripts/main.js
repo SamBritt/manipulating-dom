3e7aa2af505abd65ed646e900e342aa30e331856
SamBritt <sam.britt2@yahoo.com>
2019-02-27 11:20:42 -0600

    more practice

---
+++ src/scripts/main.js
@@ -0,0 +1,10 @@
+console.log("sup dude");
+
+/*
+    This code will only return the first section component
+    even though there are three components with the same
+    class
+*/
+const sectionEl = document.querySelector(".article__section");
+
+console.log(sectionEl);
